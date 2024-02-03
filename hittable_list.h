a0f8b73dc66e2d9f1357f43ae6c48d16bfe947da
Kaushik Mishra <kaushik.ultimate96@gmail.com>
2024-02-03 21:30:47 +0530

    Initial commit

---
+++ hittable_list.h
@@ -0,0 +1,46 @@
+#ifndef HITTABLE_LIST_H
+#define HITTABLE_LIST_H
+
+#include "hittable.h"
+
+#include <memory>
+#include <vector>
+
+using std::shared_ptr;
+using std::make_shared;
+
+//this is some generic code for storing a bunch of (ray)hittable objects efficiently. I didnt bother to understand how this code works.
+//this code can add objects to the list of hittable objects and iteratively check if any of these objects was hit.
+//the exact techinicalities of how shared_ptr works is explained in the website.
+
+class hittable_list : public hittable {
+  public:
+    std::vector<shared_ptr<hittable>> objects;
+
+    hittable_list() {}
+    hittable_list(shared_ptr<hittable> object) { add(object); }
+
+    void clear() { objects.clear(); }
+
+    void add(shared_ptr<hittable> object) {
+        objects.push_back(object);
+    }
+
+    bool hit(const ray& r, interval ray_t, hit_record& rec) const {
+        hit_record temp_rec;
+        bool hit_anything = false;
+        auto closest_so_far = ray_t.max;
+
+        for (const auto& object : objects) {
+            if (object->hit(r, interval(ray_t.min, closest_so_far), temp_rec)) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                rec = temp_rec;
+            }
+        }
+
+        return hit_anything;
+    }
+};
+
+#endif
